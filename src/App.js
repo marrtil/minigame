22a6b086a0bd7ea01073732ce186c1c5e69a0b23
marrtil <marrtil@naver.com>
2022-09-23 21:10:41 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+// import {useState} from 'react'
+import Card from './Card';
+
+function App() {
+  return (
+   <>
+  <div>
+    <Card/>
+    <Card/>
+    <Card/>
+  </div>
+  <div>
+    <Card/>
+    <Card/>
+    <Card/>
+  </div>
+   </>
+  );
+}
+
+export default App;
---
+++ src/Card.js
@@ -0,0 +1,8 @@
+import pizza from './assets/pizza.png'
+
+
+function Card(){
+    return <img alt='pizza' src={pizza}/>;
+}
+
+export default Card;
