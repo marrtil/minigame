e4c7ac04f36c8e9ef540a7d3b6a16fd78ceaef99
marrtil <marrtil@naver.com>
2022-09-25 00:08:05 +0900

    카드 16장을 랜덤으로 8쌍 생성 구현

--- src/App.js
+++ src/App.js
@@ -3,17 +3,39 @@ import './App.css';
 import Card from './Card';
 
 function App() {
+  var cardNum=16; //이건 나중에 따로 난이도 설정으로 갯수 바꿀수있게 해볼 예정
+  let num=[];
+  for(let i=0; i<cardNum; i++){
+    var ran=Math.floor(Math.random()*cardNum+1);
+    while(1){
+    if(num.includes(ran))
+    {ran=Math.floor(Math.random()*cardNum+1);}
+    else {
+      num[i]=ran;
+      break;
+    }
+    }
+  } //1~16까지의 수를 랜덤하게 배열에 넣는다
+  
+  var cardSet1=[];  //첫번째열을 위한 카드번호 배열
+  var cardSet2=[];  //두번째열을 위한 카드번호 배열
+  for(let i=0; i<num.length; i++)
+  {
+    if(i>=num.length){
+      cardSet2.push(<Card value={num[i]} />); //num의 8~15번까지의 인덱스 값을 넣음
+    }
+    else {
+      cardSet1.push(<Card value={num[i]} />); //num의 0~7번까지의 인덱스 값을 넣음
+    }
+  }
+  
   return (
    <>
   <div>
-    <Card></Card>
-    <Card/>
-    <Card/>
+    {cardSet1}
   </div>
   <div>
-    <Card/>
-    <Card/>
-    <Card/>
+    {cardSet2}
   </div>
    </>
   );
--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,37 @@
-import pizza from './assets/pizza.png'
+import pizza from './assets/피자.png';
+import banana from './assets/바나나.png';
+import cookie from './assets/쿠키.png';
+import watermel from './assets/수박.png';
+import chicken from './assets/치킨.png';
+import cheeze from './assets/치즈.png';
+import choco from './assets/초콜릿.png';
+import shirimp from './assets/새우.png';
+import './Card.css'
+
+const cardImage = {
+    1:pizza,
+    2:banana,
+    3:cookie,
+    4:watermel,
+    5:chicken,
+    6:cheeze,
+    7:choco,
+    8:shirimp,
+    9:pizza,
+    10:banana,
+    11:cookie,
+    12:watermel,
+    13:chicken,
+    14:cheeze,
+    15:choco,
+    16:shirimp,
+    
+}
 
 
-function Card(){
-    return <img alt='pizza' src={pizza}/>;
+function Card({value}){
+    const src=cardImage[value];
+    return <img alt={value} src={src}/>;
 }
 
 export default Card;
